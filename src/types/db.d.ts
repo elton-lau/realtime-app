d765da7b1f35a0efb13d5c79a2b78a81b74d48ec
Elton Lau <eltonlau94@gmail.com>
2023-07-22 21:10:26 -0400

    Completed message sending and fetching

--- src/types/db.d.ts
+++ src/types/db.d.ts
@@ -1,6 +1,26 @@
+
+
 interface User {
   name: string;
   email: string;
   image: string;
   id:  string;
+}
+
+interface Message {
+  id: string;
+  senderId: string;
+  text: string;
+  timestamp: number;
+}
+
+interface Chat {
+  id: string;
+  messages: Message[];
+}
+
+interface FriendRequest {
+  id: string;
+  senderId: string;
+  receiverId: string;
 }
